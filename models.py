c83ed9df1a8e11323eeee2c10f4211486216c0c6
Trần Tiến Thành (VNCDLL-CTPTNLVKHDL) <thanhtt39@vingroup.net>
2020-11-27 11:48:11 +0700

    add utils.py

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from utils_t import build_detection_based_model, build_regression_based_model, p
 import numpy as np
 
 class SHPE_model():
-    def __init__(self, pb_type='detection', model_name='resnest', n_kps=7, define_model=None, define_img_size=None):
+    def __init__(self, pb_type='detection', model_name='resnest', n_kps=7, metrics=None, define_model=None, define_img_size=None):
         self.pb_type = pb_type
         self.model_name = model_name
         self.n_kps = n_kps
@@ -23,11 +23,17 @@ class SHPE_model():
             self.img_size = define_img_size
         else:
             raise Exception("not support this pb_type!!!")
+        if metrics is not None:
+            self.metrics = metrics
+        else:
+            self.metrics = {}
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
 
     def train(self, loader_dict, loss_func, optimizer, lr=3e-4, use_lr_sch=False, epochs=120, ckp_dir='./checkpoint', writer=None):
-        criterion = loss_func
+        # criterion = loss_func
+        metrics = self.metrics
+        metrics['loss'] = loss_func
         optimizer = optimizer(self.model.parameters(),lr)
         if 'train' not in list(loader_dict.keys()):
             raise Exception("missing \'train\' keys in loader_dict!!!")
@@ -44,7 +50,7 @@ class SHPE_model():
             s="Epoch [{}/{}]:".format(epoch+1, epochs)
             start = time.time()
             for mode in modes:
-                running_loss = 0.0
+                running_metrics = dict.fromkeys(metrics.keys(), 0.0)
                 ova_len = loader_dict[mode].dataset.n_data
                 if mode == 'train':
                     self.model.train()
@@ -53,35 +59,41 @@ class SHPE_model():
                 for i, data in enumerate(loader_dict[mode]):
                     imgs, labels = data[0].to(self.device), data[1].to(self.device)
                     preds = self.model(imgs)
-                    loss = criterion(preds, labels)
+                    loss = metrics['loss'](preds, labels)
                     if mode == 'train':
                         optimizer.zero_grad()
                         loss.backward()
                         optimizer.step()
                     iter_len = imgs.size()[0]
-                    preds = (preds > 0.5).float()
-                    running_loss += loss.item()*iter_len
-                running_loss /= ova_len
+                    # preds = (preds > 0.5).float()
+                    running_loss = loss.item()*iter_len/ova_len
+                    for key in list(metrics.keys()):
+                        if key == 'loss':
+                            running_metrics[key] += running_loss
+                        else:
+                            running_metrics[key] += metrics[key](preds, labels).item()*iter_len/ova_len
                 if writer is not None:
-                    writer.add_scalars('loss', {mode: running_loss}, epoch)
-                s += "{}_loss {:.3f} -".format(mode, running_loss)
+                    for key in list(metrics.keys()):
+                        writer.add_scalars(key, {mode: running_metrics['key']}, epoch)
+                for key in list(metrics.keys()):
+                    s += "{}_{} {:.3f} -".format(mode, key, running_metrics[key])
             end = time.time()
             s = s[:-1] + "({:.1f}s)".format(end-start)
             print(s)
-            if writer is not None:
-                n_visual = 4
-                preds = self.predict(imgs[:n_visual])
-                imgs_new = imgs[:n_visual].cpu().numpy()
-                imgs_new = np.stack([imgs_new[:,0], imgs_new[:,1], imgs_new[:,2]], axis=-1)
-                preds = (preds*self.img_size[:2]).astype(np.int32)
-                for z, img_new in enumerate(imgs_new):
-                    cv2.polylines(img_new, [preds[z]], True, (0,0,255), 2)
-                imgs_new = np.stack([imgs_new[...,0], imgs_new[...,1], imgs_new[...,2]], axis=1)
-                imgs_new = torch.from_numpy(imgs_new)
-                img_grid = torchvision.utils.make_grid(imgs_new)
-                writer.add_image(str(epoch+1)+'_visual_img', img_grid)
-            if running_loss < best_loss or (epoch+1)%10==0:
-                best_loss = running_loss
+            # if writer is not None:
+            #     n_visual = 4
+            #     preds = self.predict(imgs[:n_visual])
+            #     imgs_new = imgs[:n_visual].cpu().numpy()
+            #     imgs_new = np.stack([imgs_new[:,0], imgs_new[:,1], imgs_new[:,2]], axis=-1)
+            #     preds = (preds*self.img_size[:2]).astype(np.int32)
+            #     for z, img_new in enumerate(imgs_new):
+            #         cv2.polylines(img_new, [preds[z]], True, (0,0,255), 2)
+            #     imgs_new = np.stack([imgs_new[...,0], imgs_new[...,1], imgs_new[...,2]], axis=1)
+            #     imgs_new = torch.from_numpy(imgs_new)
+            #     img_grid = torchvision.utils.make_grid(imgs_new)
+            #     writer.add_image(str(epoch+1)+'_visual_img', img_grid)
+            if running_metrics['loss'] < best_loss or (epoch+1)%10==0:
+                best_loss = running_metrics['loss']
                 torch.save(self.model.state_dict(), os.path.join(ckp_dir,'epoch'+str(epoch+1)+'.pt'))
                 print('new checkpoint saved!')
             if lr_sch is not None:
@@ -139,8 +151,10 @@ class SHPE_model():
                 targets = targets.cpu().numpy()
                 if self.pb_type == 'regression':
                     ova_loss += np.sum(np.abs(preds-targets))
-                else:
+                elif self.pb_type == 'detection':
                     preds = heatmap2coor(preds, self.n_kps, self.img_size)
                     targets = heatmap2coor(targets, self.n_kps, self.img_size)
                     ova_loss += np.sum(np.abs(preds-targets))
+                else:
+                    return None
         return ova_loss/(ova_len*14)
