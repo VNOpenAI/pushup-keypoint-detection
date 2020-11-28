c441595746ef17eef31de06cccd8c62e6f5370a3
Trần Tiến Thành (VNCDLL-CTPTNLVKHDL) <thanhtt39@vingroup.net>
2020-11-28 17:12:30 +0700

    add SOTA models

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 from models import SHPE_model
 from torchsummary import summary
+from simple_baselines.pose_resnet import get_pose_net
+from stacked_hourglass.posenet import PoseNet
+from deep_high_resolution.pose_hrnet import get_pose_net as gpn
+from deep_high_resolution.pose_resnet import get_pose_net as gpnn
 
-shpe_model = SHPE_model('detection', 'efficient')
-shpe_model.load_ckp('checkpoints/efficientb2_heatmap_filter_2211.pt')
-summary(shpe_model.model)
-shpe_model.pred_live()
+# shpe_model = SHPE_model('detection', 'efficient')
+# shpe_model.load_ckp('checkpoints/efficientb2_heatmap_filter_2211.pt')
+# summary(shpe_model.model)
+# shpe_model.pred_live()
+# model = get_pose_net(False)
+# model = PoseNet()
+model = gpn()
+# model = gpnn()
+summary(model)
+print(model)
